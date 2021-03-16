5a81c6812a028e3819b5bfd36e4a9567d97fecba
Tiannan <tiannansha@gmail.com>
2021-03-16 22:00:26 +0100

    finish q4.1.2 coding i think

--- src/main/scala/recommend/Recommender.scala
+++ src/main/scala/recommend/Recommender.scala
@@ -3,10 +3,10 @@ package recommend
 import org.rogach.scallop._
 import org.json4s.jackson.Serialization
 import org.apache.spark.rdd.RDD
-
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import predict.Predictor.{normalDevi, optionalPui}
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val data = opt[String](required = true)
@@ -44,13 +44,14 @@ object Recommender extends App {
   assert(personalFile.count == 1682, "Invalid personal data")
 
   // my code starts here
+
+  // *******************
+  // ***    Q4.1.1   ***
+  // *******************
   val myUserId = 944
   val myRatings = personalFile.map(l => l.split(",").map(_.trim))
     .filter(cols=>cols.length==3)
     .map(cols => Rating(myUserId, cols(0).toInt, cols(2).toDouble))  // col(0) is item id
-  println("myRatings:")
-  myRatings.collect.foreach(println)
-  println(s"myRatings.count()=${myRatings.count()}")
 
   val train = (data ++ myRatings) // training set is all the (u,i,r) available
   val toPredict = personalFile.map(l => l.split(",").map(_.trim))
@@ -64,14 +65,22 @@ object Recommender extends App {
     .map{case((u,i),rating) => (rating, -i)} // prefer higher rating and smaller itemId
     .top(5)
     .map{case(rating, negI)=>(rating, -negI)}
-  println("rPredTop5")
-  top5.foreach(println)
-  println()
-  var finalList = List[Any]()
-  for (i <- 0 until top5.length) {
-    var entryList = List[Any]()
-    entryList = entryList :+ top5(i)._2 :+ itemNameMap.lookup(top5(i)._2)(0):+ top5(i)._1
-    finalList = finalList :+ entryList
+  val finalListQ411 = top5ToFinalList(top5)
+//  var finalListQ411 = List[Any]()
+//  for (i <- 0 until top5.length) {
+//    var entryList = List[Any]()
+//    entryList = entryList :+ top5(i)._2 :+ itemNameMap.lookup(top5(i)._2)(0):+ top5(i)._1
+//    finalListQ411 = finalListQ411 :+ entryList
+//  }
+
+  def top5ToFinalList(top: Array[(Double, Int)]):List[Any] = {
+    var finalList = List[Any]()
+    for (i <- 0 until top.length) {
+      var entryList = List[Any]()
+      entryList = entryList :+ top(i)._2 :+ itemNameMap.lookup(top(i)._2)(0):+ top(i)._1
+      finalList = finalList :+ entryList
+    }
+    finalList
   }
 
   //rPred.take(10).foreach(println)
@@ -107,47 +116,91 @@ object Recommender extends App {
       }  // ((u,i), pui)
   }
 
-  // Some helper functions for the baseline method
-  // generate a prediction for (u,i) using ru_ and rbarhat_i while any of
-  // these two inputs might be None, in which case the prediction will be the global average
-  def optionalPui(ru:Option[Double], rbarhat_i:Option[Double], avgGlobal:Double):Double = {
-    //    var ruGet = avgGlobal
-    //    var rbarhat_iGet = avgGlobal
-    if (ru.isEmpty && rbarhat_i.isEmpty) {
-      return pui(avgGlobal, avgGlobal)
-    } else if (ru.isDefined && rbarhat_i.isEmpty) {
-      return pui(ru.get, avgGlobal)
-    } else if (ru.isEmpty && rbarhat_i.isDefined) {
-      return pui(avgGlobal, rbarhat_i.get)
-    } else {
-      return pui(ru.get, rbarhat_i.get)
-    }
+//  // Some helper functions for the baseline method
+//  // generate a prediction for (u,i) using ru_ and rbarhat_i while any of
+//  // these two inputs might be None, in which case the prediction will be the global average
+//  def optionalPui(ru:Option[Double], rbarhat_i:Option[Double], avgGlobal:Double):Double = {
+//    //    var ruGet = avgGlobal
+//    //    var rbarhat_iGet = avgGlobal
+//    if (ru.isEmpty && rbarhat_i.isEmpty) {
+//      return pui(avgGlobal, avgGlobal)
+//    } else if (ru.isDefined && rbarhat_i.isEmpty) {
+//      return pui(ru.get, avgGlobal)
+//    } else if (ru.isEmpty && rbarhat_i.isDefined) {
+//      return pui(avgGlobal, rbarhat_i.get)
+//    } else {
+//      return pui(ru.get, rbarhat_i.get)
+//    }
+//  }
+//
+//  // generate a prediction for (u,i) using ru_ and rbarhat_i
+//  def pui(ru:Double, rbarhat_i:Double):Double = {
+//    ru + rbarhat_i * scale((ru+rbarhat_i), ru)
+//  }
+//
+//  // normalizedDeviation
+//  def normalDevi(rui:Double, ru:Double): Double = {
+//    (rui - ru)/scale(rui, ru)
+//  }
+//
+//  def scale(x:Double, ru:Double): Double = {
+//    if (x>ru) {
+//      5-ru
+//    } else if (x<ru) {
+//      ru-1
+//    }else {
+//      1
+//    }
+//  }
+
+  // investigate the recommended items without adjust rating based on popularity
+  // (r,1) -> (r, [1,1,...]) -> (r, 1+1+...+1)
+  val countByI = train.map(r=>(r.item, 1)).reduceByKey((a,b)=>a+b)
+  val item814count = countByI.lookup(814)(0)
+  val item1122count = countByI.lookup(1122)(0)
+  val item1189count = countByI.lookup(1189)(0)
+  val item1201count = countByI.lookup(1201)(0)
+  val item1293count = countByI.lookup(1293)(0)
+  println(s"item814count=$item814count")
+  println(s"item1122count=$item1122count")
+  println(s"item1189count=$item1189count")
+  println(s"item1201count=$item1201count")
+  println(s"item1293count=$item1293count")
+
+  // *******************
+  // ***    Q4.1.2   ***
+  // *******************
+  val countByIStats = countByI.map{case(i, count) => count}.stats()
+  println(s"countByIStats=$countByIStats")
+  val adjustedTop5 = calcRPredBaselineMethod(train, toPredict)  // ((u,i), pui)
+    .map{ case((u,i),r) => (i,r) } // (i, pui)
+    .join(countByI) //(i, (pui, count_i))
+    .mapValues{case(pui, count_i) => adjustedPui(pui, count_i)}  // (i, adjustedPui)
+    .join(itemNameMap) // (i, (adjustedPui, itemName))
+    .map{case(i, (adjustedPui, itemName)) => (adjustedPui, -i)} // (adjustedPui, -i)
+    .top(5)
+    .map{case(adjustedPui, negI) => (adjustedPui, -negI)} // (adjustedPui, i)
+  val finalListQ412 = top5ToFinalList(adjustedTop5)
+
+  println("test adjusted Pui")
+  println(adjustedPui(1.0, 1))
+  println(adjustedPui(5.0, 250))
+  println(s"starwar rating count = ${countByI.lookup(50)(0)}")
+  val starwarRating = train.groupBy(r => r.item).map{
+    case (i, rs) => (i, rs.map(r=>r.rating).sum / rs.size.toDouble)
+  }.lookup(50)
+  println(s"starwarRating=$starwarRating")
+
+  def adjustedPui(pui:Double, count_i:Int):Double = {
+    // first normalize count to between (0,1], then smooth it by raising it to the power of 1/8
+    val popularityFactor = scala.math.pow(count_i.toDouble/countByIStats.max, 0.0625)
+    val adjusted = popularityFactor * pui
+    if (adjusted < 1.0) 1.0
+    else adjusted
   }
-
-  // generate a prediction for (u,i) using ru_ and rbarhat_i
-  def pui(ru:Double, rbarhat_i:Double):Double = {
-    ru + rbarhat_i * scale((ru+rbarhat_i), ru)
-  }
-
-  // normalizedDeviation
-  def normalDevi(rui:Double, ru:Double): Double = {
-    (rui - ru)/scale(rui, ru)
-  }
-
-  def scale(x:Double, ru:Double): Double = {
-    if (x>ru) {
-      5-ru
-    } else if (x<ru) {
-      ru-1
-    }else {
-      1
-    }
-  }
-
-
 
   // Save answers as JSON
-  def printToFile(content: String, 
+  def printToFile(content: String,
                   location: String = "./answers.json") =
     Some(new java.io.PrintWriter(location)).foreach{
       f => try{
@@ -155,7 +208,7 @@ object Recommender extends App {
       } finally{ f.close }
   }
   conf.json.toOption match {
-    case None => ; 
+    case None => ;
     case Some(jsonFile) => {
       var json = "";
       {
@@ -167,7 +220,8 @@ object Recommender extends App {
             // please report the top-5 recommendations that have the smallest
             // movie identifier.
 
-            "Q4.1.1" -> finalList
+            "Q4.1.1" -> finalListQ411,
+            "Q4.1.2" -> finalListQ412
 //              List[Any](
 //              List(254, "Batman & Robin (1997)", 5.0), // Datatypes for answer: Int, String, Double
 //              List(338, "Bean (1997)", 5.0),
